8bd14afb749c86ace2fd1dff38047ae55450aada
Ramiro Cosa <ramirocosa@gmail.com>
2023-05-24 09:52:26 -0300

    logica de login y servicio

--- FrontEnd/src/app/auth/auth.module.ts
+++ FrontEnd/src/app/auth/auth.module.ts
@@ -3,17 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent,
-    ForgotPasswordComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginComponent, RegisterComponent, ForgotPasswordComponent],
+  imports: [CommonModule, HttpClientModule, FormsModule, ReactiveFormsModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- FrontEnd/src/app/models/iclient.ts
+++
@@ -1,8 +0,0 @@
-export interface IClient {
-  id: number;
-  name: string;
-  account: number;
-  credit: number;
-  cvu: number;
-  alias: string;
-}
