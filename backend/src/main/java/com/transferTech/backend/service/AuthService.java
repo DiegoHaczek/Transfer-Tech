067d1ed7227cc1285d2271940a6f8b0adcd153c4
DiegoHaczek <diegohaczek1996@hotmail.com>
2023-05-29 14:39:05 -0300

    EF-19: Agregado envio mail comprobacion

--- backend/src/main/java/com/transferTech/backend/service/AuthService.java
+++ backend/src/main/java/com/transferTech/backend/service/AuthService.java
@@ -10,12 +10,14 @@ import com.transferTech.backend.exception.AlreadyExistException;
 import com.transferTech.backend.exception.NotFoundException;
 import com.transferTech.backend.mapper.AuthDtoMapper;
 import com.transferTech.backend.repository.UserRepository;
+import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +25,7 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final AccountService accountService;
+    private final EmailService emailService;
     private final AuthDtoMapper mapper;
     private final PasswordEncoder encoder;
 
@@ -32,11 +35,19 @@ public class AuthService {
         }
         User newUser = mapper.requestToEntity(request);
         newUser = userRepository.save(newUser);
+        Long verificationCode = generateVerificationCode();
+        emailService.sendVerificationEmail(newUser.getEmail(),verificationCode);
         //newUser.setAccount(accountService.createAccount(newUser));
         Map<String, Long> response = new HashMap<>();
         response.put("user_id", newUser.getId());
+        response.put("verification_code", verificationCode);
         return response;
         //return new AuthenticationResponseDto("12345");
+    }
+
+    public Long generateVerificationCode() {
+        Random rand = new Random();
+        return rand.nextLong(100000,999999);
     }
     public AuthenticationResponseDto authenticate(AuthenticationRequestDto request) {
         User user = userRepository.findByEmail(request.getEmail()).orElseThrow(() ->
