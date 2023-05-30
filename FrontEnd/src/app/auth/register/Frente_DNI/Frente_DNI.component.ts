a8304f71ec140ace689787bcb42f852f2f4ba14b
LucioBurella <lucioburella@live.com>
2023-05-30 09:08:40 -0300

    Frente_DNI

---
+++ FrontEnd/src/app/auth/register/Frente_DNI/Frente_DNI.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Frente_DNI',
+  templateUrl: './Frente_DNI.component.html',
+  styleUrls: ['./Frente_DNI.component.css']
+})
+export class Frente_DNIComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
